23e2ae0ec2a7749834cec5422cea90202aad16cb
Donnel of duskendale Bruen <dbruen@e2r9p2.21-school.ru>
2019-02-14 17:51:44 +0300

    3sc

--- ft_strncat.c
+++ ft_strncat.c
@@ -6,7 +6,7 @@
 /*   By: dbruen <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/14 16:09:53 by dbruen            #+#    #+#             */
-/*   Updated: 2019/02/14 17:44:48 by dbruen           ###   ########.fr       */
+/*   Updated: 2019/02/14 17:50:47 by dbruen           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,19 @@
 char	*ft_strncat(char *dest, char *src, int i)
 {
 	int j;
+	int k;
 
 	j = 0;
+	k = 0;
 	while (dest[j] != '\0')
 		j++;
 	if (dest[j] == '\0')
 	{
-		while (*src && i > 0)
+		while (src[k] && i > 0)
 		{
-			dest[j]	= *src++;
+			dest[j] = src[k];
 			j++;
+			k++;
 			i--;
 		}
 	}
