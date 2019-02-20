a8249e227c6653dfbe99751911de3a52e8448985
Gnmsk <gennadyartemev@gmail.com>
2019-02-20 09:32:31 +0300

    work push

--- ft_strsplit.c
+++ ft_strsplit.c
@@ -10,4 +10,57 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+
+int	ft_strlen(const char *str)
+{
+	int i;
+
+	i = 0;
+	while (*str++)
+	{
+		i++;
+	}
+	return (i);
+}
+
+char	*ft_strnew(size_t size)
+{
+	char	*newstr;
+	size_t	i;
+
+	i = 0;
+	if (size + 1 < size)
+		return (NULL);
+	if (!(newstr = (char *)malloc(size + 1)))
+		return (NULL);
+	while (newstr[i])
+	{
+		newstr[i] = 0;
+		i++;
+	}
+	return (newstr);
+}
+
+char **ft_strsplit(char const *s, char c)
+{
+	size_t a;
+	size_t b;
+	size_t len;
+	char str[a][b];
+
+	a, b, len = 0;
+	while (
+}
+
+int	main(int argc, char **argv)
+{
+	char **str;
+
+	if (argc != 3)
+		return (printf("%s", "arguments error"));
+	str = ft_strsplit(argv[2], argv[3]);
+	return (0);
+}
