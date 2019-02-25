82fbcbe5fd700cd0e555554a3f9c9a3b7eff6f4c
Gnmsk <gennadyartemev@gmail.com>
2019-02-25 19:31:11 +0300

    9push

--- ft_strmap.c
+++ ft_strmap.c
@@ -11,3 +11,20 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+
+char	*ft_strmap(char const *s, char (*f)(char))
+{
+	size_t i;
+	char *str;
+
+	i = 0;
+	if (!(str = (char *)malloc((ft_strlen(s)) + 1)))
+		return (NULL);
+	while (s[i] != '\0')
+	{
+		str[i] = f(s[i]);
+		i++;
+	}
+	str[i] = '\0';
+	return (str);
+}
--- ft_strmapi.c
+++ ft_strmapi.c
@@ -11,3 +11,21 @@
 /* ************************************************************************** */
 
 #include "libft.h"
+
+char    *ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+        size_t i;
+        char *str;
+
+        i = 0;
+        if (!(str = (char *)malloc((ft_strlen(s)) + 1)))
+                return (NULL);
+        while (s[i] != '\0')
+        {
+                str[i] = f(i, s[i]);
+                i++;
+        }
+        str[i] = '\0';
+        return (str);
+}
+
